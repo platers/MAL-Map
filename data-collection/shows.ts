5367607ee4703fd51b8f97e5793f03e43a741b3a
platers <platers81@gmail.com>
2022-01-15 03:39:05 -0500

    anilist data

--- data-collection/shows.ts
+++ data-collection/shows.ts
@@ -4,7 +4,6 @@ const cliProgress = require('cli-progress');
 import fetch from "cross-fetch";
 import { ANIME_DATA, ANIME_DICT } from './types';
 
-
 const KEY = process.env.MAL_KEY;
 
 export async function getIds() {
@@ -26,11 +25,89 @@ export async function getIds() {
     // dedup ids
     const uniqueIds = _.uniq(ids);
 
-    return ids;
+    return uniqueIds;
+}
+
+type ANILIST_RECS = {
+    [id: number]: {
+        [id: number]: number;
+    };
+}
+
+export async function storeAniListMetadata(ids: number[] = [], filename = 'data/metadata-anilist.json') {
+    const metadata: ANILIST_RECS = JSON.parse(fs.readFileSync(filename).toString());
+    console.log(`${_.size(metadata)} shows already have anilist metadata out of ${ids.length}`);
+    const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+    bar.start(ids.length - _.size(metadata), 0);
+    for (const id of ids) {
+        if (metadata[id]) {
+            continue;
+        }
+        bar.increment();
+
+        try {
+            metadata[id] = {};
+            for (let i = 1; ; i++) {
+                const res = await fetch(`https://graphql.anilist.co`, {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        query: `{
+                            Media(idMal: ${id}) {
+                              recommendations(page: ${i}) {
+                                nodes {
+                                  mediaRecommendation {
+                                    idMal
+                                  }
+                                  rating
+                                }
+                              }
+                            }
+                          }
+                        `
+                    })
+                });
+                const json = await res.json();
+                const nodes = json.data.Media.recommendations.nodes
+                    .filter(n => n.mediaRecommendation?.idMal);
+                for (const node of nodes) {
+                    const rec = node.mediaRecommendation.idMal;
+                    if (node.rating < 1) {
+                        continue;
+                    }
+                    if (!metadata[id][rec]) {
+                        metadata[id][rec] = 0;
+                    }
+                    metadata[id][rec] += node.rating;
+                    // console.log(`${id} -> ${rec}`);
+                }
+
+                await new Promise(resolve => setTimeout(resolve, 800));
+
+                if (nodes.length === 0) {
+                    break;
+                }
+            }
+
+        } catch (e) {
+            console.log(e, id);
+        }
+        await new Promise(resolve => setTimeout(resolve, 600));
+
+        if (_.size(metadata) % 20 === 1) {
+            fs.writeFileSync(filename, JSON.stringify(metadata, null, 2));
+        }
+    }
+    bar.stop();
+    fs.writeFileSync(filename, JSON.stringify(metadata, null, 2));
+    return metadata;
 }
 
 export async function storeMetadata(ids: number[] = [], filename = 'data/metadata.json') {
-    const metadata = JSON.parse(fs.readFileSync('data/metadata.json').toString());
+    const metadata = JSON.parse(fs.readFileSync(filename).toString());
     console.log(`${_.size(metadata)} shows already have metadata out of ${ids.length}`);
     const bar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
     bar.start(ids.length - _.size(metadata), 0);
@@ -99,8 +176,24 @@ function parseMetadata(json): ANIME_DATA {
     }
 }
 
-export function processMetadata(metadata) {
-    const data = _.mapValues(metadata, parseMetadata);
+export function augmentMetadata(metadata: ANIME_DICT, anilist_metadata: ANILIST_RECS) {
+    for (const id in anilist_metadata) {
+        const ani_recs = anilist_metadata[id];
+        const recs = metadata[id].recommendations;
+        for (const rec in ani_recs) {
+            const fnd = recs.find(r => r.id === rec);
+            if (fnd) {
+                fnd.count += ani_recs[rec];
+            } else {
+                recs.push({ id: rec, count: ani_recs[rec] });
+            }
+        }
+    }
+    return metadata;
+}
+
+export function processMetadata(metadata, anilist_metadata: ANILIST_RECS) {
+    const data = augmentMetadata(_.mapValues(metadata, parseMetadata), anilist_metadata);
     const filtered = filterMetadata(data);
     console.log(`${_.size(filtered)} shows filtered`);
 
