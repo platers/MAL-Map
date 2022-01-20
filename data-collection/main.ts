44beefced5c362635425f1f298609cc712e98b90
platers <platers81@gmail.com>
2022-01-20 13:03:26 -0500

    combine cluster logic

--- data-collection/main.ts
+++ data-collection/main.ts
@@ -1,17 +1,24 @@
-import { createCluster } from "./cluster";
+import { Cluster, createCluster } from "./cluster";
 import { Layout } from "./layout";
 import { storeEdges } from "./recs";
 import { storeMetadata, getIds, processMetadata, storeAniListMetadata } from "./shows";
+import { ANIME_DICT } from "./types";
 const fs = require('fs');
 
 main();
 
 async function main() {
-    const ids = await getIds();
-    const metadata_json = await storeMetadata(ids);
-    const anilist_metadata_json = await storeAniListMetadata(ids);
-    // const metadata_json = JSON.parse(fs.readFileSync('data/metadata.json').toString());
-    const metadata = processMetadata(metadata_json, anilist_metadata_json);
+    let metadata: ANIME_DICT = {};
+    if (process.argv.length === 3 && process.argv[2] === 'cached') {
+        const metadata_json = JSON.parse(fs.readFileSync('data/metadata.json').toString());
+        const anilist_metadata_json = JSON.parse(fs.readFileSync('data/metadata-anilist.json').toString());
+        metadata = processMetadata(metadata_json, anilist_metadata_json);
+    } else {
+        const ids = await getIds();
+        const metadata_json = await storeMetadata(ids);
+        const anilist_metadata_json = await storeAniListMetadata(ids);
+        metadata = processMetadata(metadata_json, anilist_metadata_json);
+    }
 
     const edges = storeEdges(metadata);
 
--- data-collection/utils.ts
+++ data-collection/utils.ts
@@ -4,20 +4,3 @@ export async function randomWait(minWait = 2000): Promise<void> {
         setTimeout(resolve, random_wait);
     });
 }
-
-/**
- * Executes a shell command and return it as a Promise.
- * @param cmd {string}
- * @return {Promise<string>}
- */
-export function execShellCommand(cmd) {
-    const exec = require('child_process').exec;
-    return new Promise((resolve, reject) => {
-        exec(cmd, (error, stdout, stderr) => {
-            if (error) {
-                console.warn(error);
-            }
-            resolve(stdout ? stdout : stderr);
-        });
-    });
-}
